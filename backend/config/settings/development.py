2f3db524db1dbabe931106998d4ba5538a6fa7e3
Giuliano Zulatto <giulianodanielzulatto@gmail.com>
2026-06-03 22:49:25 -0300

    fix: nosec B104 en ALLOWED_HOSTS development (bandit CI)

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -3,4 +3,4 @@ from .base import *  # noqa: F401,F403
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]  # noqa: S104  # nosec B104
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
